402809071f3fb6eb944ea567ff2171a8e36edb5b
LPeter1997 <lenkefi.peti@gmail.com>
2019-05-21 00:12:24 +0200

    Made Catch2 come from Conan instead of inlining

--- single_include/sample_library/sample_lib.hpp
+++ single_include/sample_library/sample_lib.hpp
@@ -4,7 +4,7 @@
  * Version: 0.0.1
  *
  * This file has been merged from multiple source files.
- * Generation date: 2019-05-20T18:05:07
+ * Generation date: 2019-05-21T00:10:48
  *
  * Copyright (c) 2019 Peter Lenkefi
  * Distributed under the MIT License.
