6fc89b6f333441be370b9ffa3941d4dac2029c4f
LPeter1997 <lenkefi.peti@gmail.com>
2019-05-21 08:02:48 +0200

    Re-added catch test discovery

--- single_include/sample_library/sample_lib.hpp
+++ single_include/sample_library/sample_lib.hpp
@@ -4,7 +4,7 @@
  * Version: 0.0.1
  *
  * This file has been merged from multiple source files.
- * Generation date: 2019-05-21T00:10:48
+ * Generation date: 2019-05-21T07:59:23
  *
  * Copyright (c) 2019 Peter Lenkefi
  * Distributed under the MIT License.
