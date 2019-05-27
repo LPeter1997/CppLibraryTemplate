6020d45b53ff15761e759e484d1d0efc1711027d
LPeter1997 <lenkefi.peti@gmail.com>
2019-05-27 10:36:21 +0200

    Made clang-tidy invocation with the python script

---
+++ cmake/FindProgramRequired.cmake
@@ -0,0 +1,18 @@
+#[[
+ # FindProgramRequired.cmake
+ #
+ # Copyright (c) 2019 Peter Lenkefi
+ # Distributed under the MIT License.
+ #
+ # A find_program wrapper to automatically fail if the program is not found.
+ #
+ # Usage:
+ # find_program_required(ARGS...)
+]]
+
+macro(find_program_required varname)
+    find_program(${ARGV})
+    if(NOT varname)
+        message(FATAL_ERROR "Could not find program ${varname}!")
+    endif()
+endmacro(find_program_required)
