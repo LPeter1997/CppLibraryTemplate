d9e99aa184f641ecd44bf8847bea46719eac0ef0
LPeter1997 <lenkefi.peti@gmail.com>
2019-05-28 13:38:00 +0200

    Update clang-tidy-check.sh

--- scripts/clang-tidy-check.sh
+++ scripts/clang-tidy-check.sh
@@ -9,10 +9,9 @@ if [[ -n $(grep "warning: " $1) ]] || [[ -n $(grep "error: " $1) ]]; then
     echoerr ""
     out=$(grep --color -E '^|warning: |error: ' $1)
     echoerr $out
+    # Because travis...
+    sleep 1
     exit -1;
 else
     echoerr -e "\033[1;32m\xE2\x9C\x93 passed:\033[0m $1";
 fi;
-
-# Because travis...
-sleep 1
