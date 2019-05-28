b40b2b95bd19a0258c832fabc64678cb9d96ec44
LPeter1997 <lenkefi.peti@gmail.com>
2019-05-28 13:08:38 +0200

    Redirected output to stderr

--- scripts/clang-tidy-check.sh
+++ scripts/clang-tidy-check.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+echoerr() { echo "$@" 1>&2; }
+
 warn_cnt=$(grep -c "warning: " $1)
 err_cnt=$(grep -c "error: " $1)
 
-echo "Warnings: ${warn_cnt}"
-echo "Errors: ${err_cnt}"
+echoerr "Warnings: ${warn_cnt}"
+echoerr "Errors: ${err_cnt}"
 
 if [ "$warn_cnt" -gt 0 ] || [ "$err_cnt" -gt 0 ]; then
-    echo "Warnings or errors in the output!"
+    echoerr "Warnings or errors in the output!"
     exit -1;
 fi;
