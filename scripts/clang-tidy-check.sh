b0916df32a40139ef477b5b2c3a44e4823dc59ad
LPeter1997 <lenkefi.peti@gmail.com>
2019-05-28 12:22:53 +0200

    Update clang-tidy-check.sh

--- scripts/clang-tidy-check.sh
+++ scripts/clang-tidy-check.sh
@@ -6,7 +6,7 @@ err_cnt=$(grep -c "error: " $1)
 echo "Warnings: ${warn_cnt}"
 echo "Errors: ${err_cnt}"
 
-if ["$warn_cnt" -gt 0] || ["$err_cnt" -gt 0]; then
+if [ "$warn_cnt" -gt 0 ] || [ "$err_cnt" -gt 0 ]; then
     echo "Warnings or errors in the output!"
     exit -1;
 fi;
