51cf178563fd15fabdec4a34b5862cc5c0c3f1ce
LPeter1997 <lenkefi.peti@gmail.com>
2019-06-06 18:28:14 +0200

    Added clang-format

---
+++ cmake/InvokeFormat.cmake
@@ -0,0 +1,51 @@
+#[[
+ # InvokeFormat.cmake
+ #
+ # Copyright (c) 2019 Peter Lenkefi
+ # Distributed under the MIT License.
+ #
+ # A CMake wrapper for the python formatter script.
+ #
+ # Usage:
+ # invoke_tidy(<target>)
+]]
+
+include(${CMAKE_SOURCE_DIR}/cmake/FindProgramRequired.cmake)
+include(${CMAKE_SOURCE_DIR}/cmake/PrefixedVariable.cmake)
+
+# Create a configuration variable for the script
+prefixed_variable(
+    FORMAT_SCRIPT FILEPATH
+    "The path of the python format script."
+    "${CMAKE_SOURCE_DIR}/scripts/formatter.py"
+)
+
+# TODO: Maybe have a version setter for the Clang tools?
+
+function(invoke_format target)
+    # Need python 3.6 for the merge script
+    find_package(PythonInterp 3.6 REQUIRED)
+
+    # Find the clang-tidy binary
+    find_program_required(CLANG_FORMAT clang-format)
+    # Find the clang-tidy helper script
+    find_program_required(CLANG_FORMAT_DIFF run-clang-format-8)
+
+    set(FORMAT_ENV_VARS
+        BRANCH_MASTER=${BRANCH_MASTER}
+        BRANCH_RELEASE=${BRANCH_RELEASE}
+        BRANCH_DEVELOPMENT=${BRANCH_DEVELOPMENT}
+    )
+
+    # Invoke the actual script
+    add_custom_command(
+        COMMAND ${CMAKE_COMMAND} -E env ${FORMAT_ENV_VARS}
+            "${PYTHON_EXECUTABLE}" ${${PROJECT_NAME}_FORMAT_SCRIPT}
+                --format-bin "${CLANG_FORMAT}"
+                --run-diff "${CLANG_FORMAT_DIFF}"
+        OUTPUT ".nonesuch__" # Just so we always run
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        VERBATIM
+    )
+    add_custom_target(${target} ALL DEPENDS ".nonesuch__")
+endfunction(invoke_format)
---
+++ scripts/formatter.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+"""
+ " formatter.py
+ "
+ " Copyright (c) 2019 Peter Lenkefi
+ " Distributed under the MIT License.
+ "
+ " A Python script to invoke clang-format depending on the branch.
+ " On a dev branch we invoke clang-format-diff.py and compare to master. On the
+ " master branch we simply call clang-format to fully format.
+"""
+
+import argparse
+import os
+import sys
+
+from common import *
+
+# Script version
+VER_MAJOR = 0
+VER_MINOR = 1
+VER_PATCH = 0
+
+__version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_PATCH}'
+
+def run_full_format(script):
+    retval = os.system(f'{script} > format_out.txt')
+    with open('format_out.txt', 'r') as f:
+        content = f.read()
+        #m = re.match(r'Enabled checks:(\r\n?|\n)(\s+.*(\r\n?|\n))*', content)
+        #end = len(m.group())
+        #content = content[end :]
+        print(content)
+    retval = os.WEXITSTATUS(retval)
+    sys.exit(retval)
+
+def run_diff_format(script):
+    # TODO
+    os.system('')
+
+def run_format_branch(full_format, diff_format):
+    matched, projver = is_rel_branch()
+    matched2, projver = is_master_branch()
+    if matched or matched2:
+        # Run full
+        print('On master or release branch, running full format!')
+        run_full_format(full_format)
+    else:
+        # Run diff
+        print('Not on master or release branch, running differential format!')
+        run_diff_format(diff_format)
+
+def main():
+    # Setting up command-line arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-v", "--version",
+        action="version",
+        version=f'%(prog)s {__version__}')
+
+    # Optional arguments
+    #parser.add_argument("-t", "--target",
+    #    help="specifies the destination file where the merge happens",
+    #    default="merged_header.hpp")
+    #parser.add_argument("-e", "--exclude",
+    #    help="specifies paths and files to exclude when merging",
+    #    action='append',
+    #    default=[])
+
+    # Required arguments
+    required_args = parser.add_argument_group('required arguments')
+    required_args.add_argument("-b", "--format-bin",
+        help="specifies the path to the clang-format binary",
+        required=True)
+    required_args.add_argument("-d", "--run-diff",
+        help="specifies the path to the clang-format-diff.py script",
+        required=True)
+
+    args = parser.parse_args()
+
+    # We have all the arguments needed
+    run_format_branch(args.format_bin, args.run_diff)
+
+# Start execution in main()
+if __name__ == "__main__":
+    main()
